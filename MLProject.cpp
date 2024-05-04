a67a0fcb67caece49276056752da39b79dd55d1c
Saeed Ayman <saeed20359@gmail.com>
2024-05-04 10:26:18 +0300

    clean main v0.2

--- MLProject.cpp
+++ MLProject.cpp
@@ -1,61 +1,49 @@
-
 #include <iostream>
 #include <exception>
 
-#include "Distance.h"
 #include "CSV.h"
 #include "KMeansExample.h"
-#include "Agglomerative.h"
+#include "AgglomerativeExample.h"
+
+std::vector <std::vector<double>> testData1 = {
+	{1,1},
+	{1.5,1.5},
+	{5,5},
+	{3,4},
+	{4,4},
+	{3,3.5}
+};
+
+std::vector <std::vector<double>> testData2 = {
+	{0.7, 3.2},
+	{2.45, 2.89},
+	{3.47, 1.12},
+	{5.23, 5.24},
+	{5.98, 6.23},
+	{7.778, 8.63},
+	{8.97, 6.12}
+};
 
 int main()
 {
 	try {
-		CSV csv("Resources/force.csv");
+		CSV csv("Resources/sample2d.csv");
 		auto data = csv.data();
+		std::cout << "~--~--~--~" << std::endl;
+		std::cout << "| KMeans |" << std::endl;
+		std::cout << "~--~--~--~" << std::endl;
+		KMeans::KMeansExample::run1(data, 10, INT_MAX /*, Distance::Euclidean*/);
+		std::cout << "-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-" << std::endl;
+		KMeans::KMeansExample::run2(data, 10, INT_MAX /*, Distance::Euclidean*/);
+		std::cout << "-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-__-" << std::endl;
 		
-		//std::vector <std::vector<double>> data = /*{
-		//	{1,1},
-		//	{1.5,1.5},
-		//	{5,5},
-		//	{3,4},
-		//	{4,4},
-		//	{3,3.5}
-		//};*/
+		std::cout << "Next Agglomerative ";
+		std::system("pause");
 
-		// {
-		//	{0.7, 3.2},
-		//	{2.45, 2.89},
-		//	{3.47, 1.12},
-		//	{5.23, 5.24},
-		//	{5.98, 6.23},
-		//	{7.778, 8.63},
-		//	{8.97, 6.12}
-		//};
-		
-		
-		//KMeans::KMeansExample::run(
-		//	KMeans::KMeans(Distance::Euclidean, 10, 30), 
-		//	data
-		//);
-		
-		
-		Agglomerative::Agglomerative agg(Distance::Euclidean);
-		
-		
-		int i = 0;
-
-		for (const auto& labels : agg.fitPredict(std::vector<std::vector<double>>(data.begin(), data.begin() + 5000))) {
-			std::cout << "_--_--_--_--_--_--_--_--_--_--_--_--_--_--_--_--_--_--_" << std::endl;
-			std::cout << "step(" << ++i << "):" << std::endl;
-			std::cout << "_--_--_--_--_--_--_--_--_--_--_--_--_--_--_--_--_--_--_" << std::endl;
-
-			for (int j = 0; j < labels.size(); j++)
-			{
-				std::cout << "cluster(" << j + 1 << "): [" << labels[j] << "]" << std::endl;
-			}
-
-			std::cout << "==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==+==" << std::endl;
-		}
+		std::cout << "~--~--~--~--~--~" << std::endl;
+		std::cout << "| Agglomerative |" << std::endl;
+		std::cout << "~--~--~--~--~--~" << std::endl;
+		Agglomerative::AgglomerativeExample::run1(data);
 	}
 	catch (std::exception e) {
 		std::cout << e.what();
